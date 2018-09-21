a644f11cc38843806915abdd832ffdf59998876d
mchalain <marc.chalain@gmail.com>
2018-09-21 09:03:06 +0200

    rc and sink on socket unix not tested

---
+++ src/sink_unix.c
@@ -0,0 +1,176 @@
+/*****************************************************************************
+ * sink_alsa.c
+ * this file is part of https://github.com/ouistiti-project/putv
+ *****************************************************************************
+ * Copyright (C) 2016-2017
+ *
+ * Authors: Marc Chalain <marc.chalain@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *****************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/ioctl.h>
+
+#include "player.h"
+#include "jitter.h"
+#include "filter.h"
+typedef struct sink_s sink_t;
+typedef struct sink_ctx_s sink_ctx_t;
+struct sink_ctx_s
+{
+	player_ctx_t *player;
+	const sink_t *ops;
+	const char *filepath;
+	pthread_t thread;
+	jitter_t *in;
+	state_t state;
+	char *out;
+	size_t length;
+	unsigned int samplerate;
+	int samplesize;
+	int nchannels;
+	pthread_cond_t event;
+	pthread_mutex_t mutex;
+	int counter;
+};
+#define SINK_CTX
+#include "sink.h"
+#include "unix_server.h"
+
+#define err(format, ...) fprintf(stderr, "\x1B[31m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#define warn(format, ...) fprintf(stderr, "\x1B[35m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#ifdef DEBUG
+#define dbg(format, ...) fprintf(stderr, "\x1B[32m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define sink_dbg(...)
+
+static const char *jitter_name = "unix socket";
+static sink_ctx_t *sink_init(player_ctx_t *player, const char *filepath)
+{
+	int count = 2;
+	jitter_format_t format = SINK_BITSSTREAM;
+	sink_ctx_t *ctx = calloc(1, sizeof(*ctx));
+
+	ctx->ops = sink_unix;
+	ctx->filepath = filepath;
+
+	pthread_mutex_init(&ctx->mutex, NULL);
+	pthread_cond_init(&ctx->event, NULL);
+
+	unsigned int size;
+	jitter_t *jitter = jitter_scattergather_init(jitter_name, 6, size);
+	jitter->ctx->frequence = DEFAULT_SAMPLERATE;
+	jitter->ctx->thredhold = 2;
+	jitter->format = format;
+	ctx->in = jitter;
+
+	ctx->player = player;
+
+	return ctx;
+}
+
+static jitter_t *sink_jitter(sink_ctx_t *ctx)
+{
+	return ctx->in;
+}
+
+static int sink_unxiclient(thread_info_t *info)
+{
+	sink_ctx_t *ctx = (sink_ctx_t *)info->userctx;
+	int ret = 1;
+	int counter = 0;
+	while (ret > 0)
+	{
+		pthread_mutex_lock(&ctx->mutex);
+		while (ctx->out == NULL && ctx->counter <= counter)
+		{
+			pthread_cond_wait(&ctx->event, &ctx->mutex);
+		}
+		counter = ctx->counter;
+		pthread_mutex_unlock(&ctx->mutex);
+		ret = send(info->sock, ctx->out, ctx->length, MSG_NOSIGNAL);
+	}
+	return ret;
+}
+
+static void *sink_thread(void *arg)
+{
+	sink_ctx_t *ctx = (sink_ctx_t *)arg;
+	int run = 1;
+
+	/* start decoding */
+	while (run)
+	{
+		unsigned char *buff = ctx->in->ops->peer(ctx->in->ctx);
+		if (buff == NULL)
+		{
+			run = 0;
+			break;
+		}
+		pthread_mutex_lock(&ctx->mutex);
+		ctx->length = ctx->in->ops->length(ctx->in->ctx);
+		memcpy(ctx->out, buff, ctx->length);
+		ctx->counter++;
+		pthread_mutex_unlock(&ctx->mutex);
+		pthread_cond_broadcast(&ctx->event);
+		ctx->in->ops->pop(ctx->in->ctx, ctx->length);
+	}
+	dbg("sink: thread end");
+	return NULL;
+}
+
+static int sink_run(sink_ctx_t *ctx)
+{
+	pthread_create(&ctx->thread, NULL, sink_thread, ctx);
+	unixserver_run(sink_unxiclient, ctx, ctx->filepath);
+	return 0;
+}
+
+static void sink_destroy(sink_ctx_t *ctx)
+{
+	pthread_join(ctx->thread, NULL);
+	jitter_scattergather_destroy(ctx->in);
+	free(ctx);
+}
+
+const sink_t *sink_unix = &(sink_t)
+{
+	.init = sink_init,
+	.jitter = sink_jitter,
+	.run = sink_run,
+	.destroy = sink_destroy,
+};
+
+#ifndef SINK_GET
+#define SINK_GET
+const sink_t *sink_get(sink_ctx_t *ctx)
+{
+	return ctx->ops;
+}
+#endif
---
+++ src/src_unix.c
@@ -0,0 +1,161 @@
+/*****************************************************************************
+ * src_alsa.c
+ * this file is part of https://github.com/ouistiti-project/putv
+ *****************************************************************************
+ * Copyright (C) 2016-2017
+ *
+ * Authors: Marc Chalain <marc.chalain@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *****************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/ioctl.h>
+
+#include "player.h"
+#include "jitter.h"
+#include "filter.h"
+typedef struct src_s src_t;
+typedef struct src_ctx_s src_ctx_t;
+struct src_ctx_s
+{
+	player_ctx_t *player;
+	const src_t *ops;
+	int handle;
+	state_t state;
+	pthread_t thread;
+	jitter_t *out;
+	unsigned int samplerate;
+};
+#define SRC_CTX
+#include "src.h"
+
+#define err(format, ...) fprintf(stderr, "\x1B[31m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#define warn(format, ...) fprintf(stderr, "\x1B[35m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#ifdef DEBUG
+#define dbg(format, ...) fprintf(stderr, "\x1B[32m"format"\x1B[0m\n",  ##__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define src_dbg(...)
+
+static const char *jitter_name = "unxi socket";
+static src_ctx_t *src_init(player_ctx_t *player, const char *filepath)
+{
+	int count = 2;
+	src_ctx_t *ctx = calloc(1, sizeof(*ctx));
+
+	ctx->ops = src_unix;
+	ctx->player = player;
+
+	ctx->handle = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (ctx->handle == 0)
+	{
+		free(ctx);
+		return NULL;
+	}
+	struct sockaddr_un addr;
+	memset(&addr, 0, sizeof(struct sockaddr_un));
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path, filepath, sizeof(addr.sun_path) - 1);
+
+	int ret = connect(ctx->handle, (struct sockaddr *) &addr, sizeof(addr));
+	if (ret < 0)
+	{
+		close(ctx->handle);
+		free(ctx);
+		return NULL;
+	}
+	return ctx;
+}
+
+static void *src_thread(void *arg)
+{
+	src_ctx_t *ctx = (src_ctx_t *)arg;
+	int ret;
+	while (ctx->state != STATE_ERROR)
+	{
+		if (player_waiton(ctx->player, STATE_PAUSE) < 0)
+		{
+			if (player_state(ctx->player, STATE_UNKNOWN) == STATE_ERROR)
+			{
+				ctx->state = STATE_ERROR;
+				continue;
+			}
+		}
+
+		unsigned char *buff = ctx->out->ops->pull(ctx->out->ctx);
+		ret = recv(ctx->handle, buff, ctx->out->ctx->size, MSG_NOSIGNAL);
+		if (ret == -EPIPE)
+		{
+		}
+		if (ret < 0)
+		{
+			ctx->state = STATE_ERROR;
+			err("sink: error write pcm %d", ret);
+		}
+		else if (ret == 0)
+		{
+			ctx->out->ops->flush(ctx->out->ctx);
+		}
+		else
+		{
+			src_dbg("sink: play %d", ret);
+			ctx->out->ops->push(ctx->out->ctx, ret, NULL);
+		}
+	}
+	dbg("sink: thread end");
+	return NULL;
+}
+
+static int src_run(src_ctx_t *ctx, jitter_t *jitter)
+{
+	ctx->out = jitter;
+	pthread_create(&ctx->thread, NULL, src_thread, ctx);
+	return 0;
+}
+
+static void src_destroy(src_ctx_t *ctx)
+{
+	pthread_join(ctx->thread, NULL);
+	close(ctx->handle);
+	free(ctx);
+}
+
+const src_t *src_unix = &(src_t)
+{
+	.init = src_init,
+	.run = src_run,
+	.destroy = src_destroy,
+};
+
+#ifndef SRC_GET
+#define SRC_GET
+const src_t *src_get(src_ctx_t *ctx)
+{
+	return ctx->ops;
+}
+#endif

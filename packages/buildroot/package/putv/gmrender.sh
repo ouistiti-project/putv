1b8ada6344616b09499691a9d8335c079b801cd4
Marc Chalain <marc.chalain@gmail.com>
2020-07-07 19:52:14 +0200

    [package] update initialization's scripts on BR

--- packages/buildroot/package/putv/gmrender.sh
+++ packages/buildroot/package/putv/gmrender.sh
@@ -1,69 +1,67 @@
 #!/bin/sh
 #
-# Starts putv.
+# Starts upnprender.
 #
 
-SYSCONFDIR=/etc/
-RUNDIR=/var/run
-SBINDIR=/usr/sbin
-BINDIR=/usr/bin
-LIBDIR=/usr/lib
-
-. /etc/setting.d/putv.in
-. /etc/setting.d/wifiap.in
-
-export WEBSOCKETDIR
-
 DAEMON=upnprender
+PIDFILE="/var/run/$DAEMON.pid"
+WEBSOCKETDIR="/var/run/websocket"
+
 OPTIONS="-o putv"
 OPTIONS="${OPTIONS} -I 192.168.10.254"
 
+[ -r "/etc/default/$DAEMON" ] && . "/etc/default/$DAEMON"
+
+export WEBSOCKETDIR
+
 start() {
-	sleep 2
+	OPTIONS="${OPTIONS} -P ${PIDFILE}"
+	OPTIONS="${OPTIONS} -d"
+
+	sleep 1
 	route add -net 224.0.0.0 netmask 240.0.0.0 $WIFI_DEVICE
-	printf "Starting UPnP renderer: "
-	echo ${BINDIR}/${DAEMON} -d -P ${RUNDIR}/${DAEMON}.pid ${OPTIONS}
-	${BINDIR}/${DAEMON} -d -P ${RUNDIR}/${DAEMON}.pid ${OPTIONS}
-	if [ $? -eq 0 ]; then
+
+	if [ -e /var/run/gpiod.pid ] && [ -z "$MDEV" ]; then
+		exit 0
+	fi
+	printf 'Starting %s: ' "$DAEMON"
+	start-stop-daemon -S -q -x "/usr/bin/$DAEMON" \
+		-- $OPTIONS
+	status=$?
+	if [ "$status" -eq 0 ]; then
 		echo "OK"
 	else
-		echo "KO"
+		echo "FAIL"
 	fi
+	return "$status"
 }
+
 stop() {
-	printf "Stopping UPnP renderer: "
-	if [ -e ${RUNDIR}/${DAEMON}.pid ]; then
-		start-stop-daemon -K -q -p ${RUNDIR}/${DAEMON}.pid
+	printf 'Stopping %s: ' "$DAEMON"
+	start-stop-daemon -K -q -p "$PIDFILE"
+	status=$?
+	if [ "$status" -eq 0 ]; then
+		rm -f "$PIDFILE"
+		echo "OK"
+	else
+		echo "FAIL"
 	fi
-	echo "OK"
+	return "$status"
 }
+
 restart() {
 	stop
+	sleep 1
 	start
 }
 
 case "$1" in
-  rising)
-    start
-    ;;
-  start)
-    if [ ! -e ${RUNDIR}/gpiod.pid ]; then
-      start
-    fi
-    ;;
-  falling)
-    stop
-    ;;
-  stop)
-    stop
-    ;;
-  restart|reload)
-    restart
-    ;;
-  *)
-    echo "Usage: $0 {start|stop|restart}"
-    exit 1
+	start|stop|restart)
+		"$1";;
+	reload)
+		# Restart, since there is no true "reload" feature.
+		restart;;
+	*)
+		echo "Usage: $0 {start|stop|restart|reload}"
+		exit 1
 esac
-
-exit $?
-
